c3bab352ed9c75110bd1833f5d224331f70db83d
Elza <aez7@ya.ru>
2022-11-11 19:48:23 +0300

    index-1

--- js/main.js
+++ js/main.js
@@ -7,14 +7,11 @@
     let array = JSON.parse(requestObj.responseText);
 
     for (let i = 0; i < array['data'].length; i++ ) {
- 
-        document.getElementById('photos').innerHTML += `
-                                    <h1>
-                                        ${array['data'][i]['attributes']['titles']['en_jp']}
-                                    </h1>
-                                    <img src='${array['data'][i]['attributes']['posterImage']['small']}'>
-                                    `;
-     
+        document.getElementById('titles').innerHTML += `<h1>${array['data'][i]['attributes']['titles']['en_jp']}</h1> 
+                                                        <img src='${array['data'][i]['attributes']['posterImage']['small']}'>
+                                                        ${array['data'][i]['attributes']['averageRating']}`;
+        
+        document.getElementById('episode').innerHTML += `${array['data'][i]['attributes']['episodeCount']}`
     }
      
   
